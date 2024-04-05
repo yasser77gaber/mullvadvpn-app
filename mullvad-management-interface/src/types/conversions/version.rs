47a05fbaf56644ff45cbf2202426466b6f8336b8
Sebastian Holmin <sebastian.holmin@mullvad.net>
2024-04-05 14:30:23 +0200

    temp

--- mullvad-management-interface/src/types/conversions/version.rs
+++ mullvad-management-interface/src/types/conversions/version.rs
@@ -1,23 +1,43 @@
 use crate::types::proto;
 
+trait IntoProto<T> {
+    fn into_proto(self) -> T;
+}
+
+impl IntoProto<proto::AppVersionInfo> for mullvad_types::version::AppVersionInfo {
+    fn into_proto(self) -> proto::AppVersionInfo {
+        proto::AppVersionInfo {
+            supported: self.supported,
+            latest_stable: self.latest_stable,
+            latest_beta: self.latest_beta,
+            suggested_upgrade: self.suggested_upgrade,
+        }
+    }
+}
+
+trait FromProto<T> {
+    fn from_proto(value: T) -> Self;
+}
+
+impl FromProto<proto::AppVersionInfo> for mullvad_types::version::AppVersionInfo {
+    fn from_proto(value: proto::AppVersionInfo) -> mullvad_types::version::AppVersionInfo {
+        Self {
+            supported: value.supported,
+            latest_stable: value.latest_stable,
+            latest_beta: value.latest_beta,
+            suggested_upgrade: value.suggested_upgrade,
+        }
+    }
+}
+
 impl From<mullvad_types::version::AppVersionInfo> for proto::AppVersionInfo {
     fn from(version_info: mullvad_types::version::AppVersionInfo) -> Self {
-        Self {
-            supported: version_info.supported,
-            latest_stable: version_info.latest_stable,
-            latest_beta: version_info.latest_beta,
-            suggested_upgrade: version_info.suggested_upgrade,
-        }
+        version_info.into_proto()
     }
 }
 
 impl From<proto::AppVersionInfo> for mullvad_types::version::AppVersionInfo {
     fn from(version_info: proto::AppVersionInfo) -> Self {
-        Self {
-            supported: version_info.supported,
-            latest_stable: version_info.latest_stable,
-            latest_beta: version_info.latest_beta,
-            suggested_upgrade: version_info.suggested_upgrade,
-        }
+        mullvad_types::version::AppVersionInfo::from_proto(version_info)
     }
 }
