849a52c4901b534a0bdecda43249b2a924262404
Emīls <emils@mullvad.net>
2024-06-13 17:40:24 +0200

    Don't set a default platform

--- ci/ios/test-router/router-config.nix
+++ ci/ios/test-router/router-config.nix
@@ -43,7 +43,7 @@ in
 
   system.stateVersion = "23.11";
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   systemd.network.netdevs."1-lanBridge" = {
     netdevConfig = {
